126a23bab2f349a2c37e3dda86429c76e76d8f76
Supervagrant <supertramp@tut.by>
2017-02-19 17:50:52 +0300

    До минифицирования и пропуска через автопрефиксер

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+var link = document.querySelector(".popup-button");
+      
+var popup = document.querySelector(".popup-form");
+      
+var login = popup.querySelector("[name=date-arrival]");
+      
+link.addEventListener("click", function(event) {
+  event.preventDefault();
+  popup.classList.toggle("modal-content-show");
+  login.focus();
+});
